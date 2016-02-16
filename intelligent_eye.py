2b6ec21f5674dfe1d2cb87fd8b43160ea72103a4
Jinyi Zhang <zhan1182@purdue.edu>
2016-02-16 17:54:26 -0500

    import fix

--- intelligent_eye.py
+++ intelligent_eye.py
@@ -5,13 +5,11 @@ __author__ = 'Jinyi'
 import os
 import sys
 
-# from Intelligent_Eyes.ui.intelligent_eye_GUI import *
-import Intelligent_Eyes.src.client
+from ui.intelligent_eye_GUI import *
+import src.client
 
 # from PySide.QtCore import *
 # from PySide.QtGui import *
-
-
 
 
 if __name__ == '__main__':
