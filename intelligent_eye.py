5e0953d57ff7552720f647fd606ca4c1e36db2ac
Jinyi Zhang <zhan1182@purdue.edu>
2016-02-16 17:36:29 -0500

    restruct code base

---
+++ intelligent_eye.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+__author__ = 'Jinyi'
+
+import os
+import sys
+
+# from Intelligent_Eyes.ui.intelligent_eye_GUI import *
+import Intelligent_Eyes.src.client
+
+# from PySide.QtCore import *
+# from PySide.QtGui import *
+
+
+
+
+if __name__ == '__main__':
+
+	pass
+
