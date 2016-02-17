1d4e0222fac10dfa66b89b3555acd7b99a0eb9e5
Jinyi Zhang <zhan1182@purdue.edu>
2016-02-16 22:18:35 -0500

    GUI set up

--- intelligent_eye.py
+++ intelligent_eye.py
@@ -5,14 +5,27 @@ __author__ = 'Jinyi'
 import os
 import sys
 
-from ui.intelligent_eye_GUI import *
 import src.client
 
-# from PySide.QtCore import *
-# from PySide.QtGui import *
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+from ui.intelligent_eye_GUI import *
+
+class Intelligent_Eye(QMainWindow, Ui_MainWindow):
+
+    def __init__(self, parent=None):
+
+        super(Intelligent_Eye, self).__init__(parent)
+        self.setupUi(self)
 
 
 if __name__ == '__main__':
 
-	pass
+	currentApp = QApplication(sys.argv)
+	currentForm = Intelligent_Eye()
+
+	currentForm.show()
+	currentApp.exec_()
+
 
--- ui/intelligent_eye_GUI.py
+++ ui/intelligent_eye_GUI.py
@@ -2,61 +2,47 @@
 
 # Form implementation generated from reading ui file 'intelligent_eye_GUI.ui'
 #
-# Created: Tue Feb 16 16:41:48 2016
-#      by: PyQt4 UI code generator 4.10.4
+# Created: Tue Feb 16 22:16:35 2016
+#      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PySide import QtCore, QtGui
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.centralwidget.setObjectName("centralwidget")
         self.btn_start = QtGui.QPushButton(self.centralwidget)
         self.btn_start.setGeometry(QtCore.QRect(130, 400, 98, 27))
-        self.btn_start.setObjectName(_fromUtf8("btn_start"))
+        self.btn_start.setObjectName("btn_start")
         self.btn_stop = QtGui.QPushButton(self.centralwidget)
         self.btn_stop.setGeometry(QtCore.QRect(130, 470, 98, 27))
-        self.btn_stop.setObjectName(_fromUtf8("btn_stop"))
+        self.btn_stop.setObjectName("btn_stop")
         self.btn_takePics = QtGui.QPushButton(self.centralwidget)
         self.btn_takePics.setGeometry(QtCore.QRect(280, 400, 98, 27))
-        self.btn_takePics.setObjectName(_fromUtf8("btn_takePics"))
+        self.btn_takePics.setObjectName("btn_takePics")
         self.btn_navigate = QtGui.QPushButton(self.centralwidget)
         self.btn_navigate.setGeometry(QtCore.QRect(440, 400, 98, 27))
-        self.btn_navigate.setObjectName(_fromUtf8("btn_navigate"))
+        self.btn_navigate.setObjectName("btn_navigate")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 25))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.btn_start.setText(_translate("MainWindow", "Start Preview", None))
-        self.btn_stop.setText(_translate("MainWindow", "Stop Preview", None))
-        self.btn_takePics.setText(_translate("MainWindow", "Take Pics", None))
-        self.btn_navigate.setText(_translate("MainWindow", "Navigate", None))
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_start.setText(QtGui.QApplication.translate("MainWindow", "Start Preview", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_stop.setText(QtGui.QApplication.translate("MainWindow", "Stop Preview", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_takePics.setText(QtGui.QApplication.translate("MainWindow", "Take Pics", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_navigate.setText(QtGui.QApplication.translate("MainWindow", "Navigate", None, QtGui.QApplication.UnicodeUTF8))
 
